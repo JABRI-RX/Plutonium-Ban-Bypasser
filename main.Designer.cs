12e23911a9260d91c29855ef513b9ec02fcdc7bb
salah jabri <salahjabri12@gmail.com>
2023-03-06 19:47:57 +0100

    Add project files.

---
+++ main.Designer.cs
@@ -0,0 +1,215 @@
+﻿
+namespace Plutonium_Ban_Bypass
+{
+    partial class main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.cma_btn = new System.Windows.Forms.Button();
+            this.cdi_btn = new System.Windows.Forms.Button();
+            this.ccn_btn = new System.Windows.Forms.Button();
+            this.cguid_btn = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.drive_result = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.cname_textbox = new System.Windows.Forms.TextBox();
+            this.guidbox = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Nirmala UI", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(283, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(327, 37);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Plutonium Ban Bypasser";
+            // 
+            // cma_btn
+            // 
+            this.cma_btn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(44)))), ((int)(((byte)(52)))));
+            this.cma_btn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.cma_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cma_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cma_btn.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.cma_btn.Location = new System.Drawing.Point(129, 138);
+            this.cma_btn.Name = "cma_btn";
+            this.cma_btn.Size = new System.Drawing.Size(240, 54);
+            this.cma_btn.TabIndex = 6;
+            this.cma_btn.Text = "Change Mac Address";
+            this.cma_btn.UseVisualStyleBackColor = false;
+            this.cma_btn.Click += new System.EventHandler(this.cma_btn_Click);
+            // 
+            // cdi_btn
+            // 
+            this.cdi_btn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(44)))), ((int)(((byte)(52)))));
+            this.cdi_btn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.cdi_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cdi_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cdi_btn.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.cdi_btn.Location = new System.Drawing.Point(129, 215);
+            this.cdi_btn.Name = "cdi_btn";
+            this.cdi_btn.Size = new System.Drawing.Size(240, 54);
+            this.cdi_btn.TabIndex = 7;
+            this.cdi_btn.Text = "Change Drive ID";
+            this.cdi_btn.UseVisualStyleBackColor = false;
+            this.cdi_btn.Click += new System.EventHandler(this.cdi_btn_Click);
+            // 
+            // ccn_btn
+            // 
+            this.ccn_btn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(44)))), ((int)(((byte)(52)))));
+            this.ccn_btn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.ccn_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ccn_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ccn_btn.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.ccn_btn.Location = new System.Drawing.Point(129, 292);
+            this.ccn_btn.Name = "ccn_btn";
+            this.ccn_btn.Size = new System.Drawing.Size(240, 54);
+            this.ccn_btn.TabIndex = 8;
+            this.ccn_btn.Text = "Change Computer Name";
+            this.ccn_btn.UseVisualStyleBackColor = false;
+            this.ccn_btn.Click += new System.EventHandler(this.ccn_btn_Click);
+            // 
+            // cguid_btn
+            // 
+            this.cguid_btn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(44)))), ((int)(((byte)(52)))));
+            this.cguid_btn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.cguid_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cguid_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cguid_btn.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.cguid_btn.Location = new System.Drawing.Point(129, 369);
+            this.cguid_btn.Name = "cguid_btn";
+            this.cguid_btn.Size = new System.Drawing.Size(240, 54);
+            this.cguid_btn.TabIndex = 9;
+            this.cguid_btn.Text = "Change GUID";
+            this.cguid_btn.UseVisualStyleBackColor = false;
+            this.cguid_btn.Click += new System.EventHandler(this.cguid_btn_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Plutonium_Ban_Bypass.Properties.Resources.pluto_logo;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(124, 111);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            // 
+            // drive_result
+            // 
+            this.drive_result.AutoSize = true;
+            this.drive_result.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.drive_result.Location = new System.Drawing.Point(404, 138);
+            this.drive_result.Name = "drive_result";
+            this.drive_result.Size = new System.Drawing.Size(0, 18);
+            this.drive_result.TabIndex = 11;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Plutonium_Ban_Bypass.Properties.Resources.pluto_logo;
+            this.pictureBox2.Location = new System.Drawing.Point(744, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(124, 111);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 12;
+            this.pictureBox2.TabStop = false;
+            // 
+            // cname_textbox
+            // 
+            this.cname_textbox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(44)))), ((int)(((byte)(52)))));
+            this.cname_textbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cname_textbox.ForeColor = System.Drawing.Color.White;
+            this.cname_textbox.Location = new System.Drawing.Point(384, 292);
+            this.cname_textbox.MaxLength = 20;
+            this.cname_textbox.Multiline = true;
+            this.cname_textbox.Name = "cname_textbox";
+            this.cname_textbox.Size = new System.Drawing.Size(317, 54);
+            this.cname_textbox.TabIndex = 13;
+            this.cname_textbox.Text = "Don\'t Add Any Symbols ";
+            // 
+            // guidbox
+            // 
+            this.guidbox.AllowDrop = true;
+            this.guidbox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(44)))), ((int)(((byte)(52)))));
+            this.guidbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guidbox.ForeColor = System.Drawing.Color.White;
+            this.guidbox.Location = new System.Drawing.Point(384, 384);
+            this.guidbox.MaxLength = 20;
+            this.guidbox.Multiline = true;
+            this.guidbox.Name = "guidbox";
+            this.guidbox.Size = new System.Drawing.Size(484, 29);
+            this.guidbox.TabIndex = 14;
+            this.guidbox.Text = "Don\'t Add Any Symbols ";
+            // 
+            // main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(44)))), ((int)(((byte)(52)))));
+            this.ClientSize = new System.Drawing.Size(880, 454);
+            this.Controls.Add(this.guidbox);
+            this.Controls.Add(this.cname_textbox);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.drive_result);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.cguid_btn);
+            this.Controls.Add(this.ccn_btn);
+            this.Controls.Add(this.cdi_btn);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cma_btn);
+            this.ForeColor = System.Drawing.Color.White;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "main";
+            this.Text = "Plutonium Ban Bypasser";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button cma_btn;
+        private System.Windows.Forms.Button cdi_btn;
+        private System.Windows.Forms.Button ccn_btn;
+        private System.Windows.Forms.Button cguid_btn;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label drive_result;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.TextBox cname_textbox;
+        private System.Windows.Forms.TextBox guidbox;
+    }
+}
+
---
+++ main.cs
@@ -0,0 +1,132 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Management;
+ 
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Plutonium_Ban_Bypass
+{
+    public partial class main : Form
+    {
+
+        [DllImport("Kernel32.dll")]
+        static extern bool SetComputerNameA(string lpComputerName);
+        public main()
+        {
+            InitializeComponent();
+        }
+         
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cname_textbox.Text = Environment.MachineName;
+            drive_result.Text = "Make Sure you run the program as admin otherwise shit won't work";
+            string key = "HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Cryptography";
+            string r = (string)Registry.GetValue(key, "MachineGuid", (object)"default");
+            guidbox.Text = "Your MachineGuid : "+r;
+            
+        }
+
+        private void cma_btn_Click(object sender, EventArgs e)
+        {
+            ProcessStartInfo info = new ProcessStartInfo("https://www.youtube.com/watch?v=9E93Q4JRZeI");
+            Process.Start(info);
+            cma_btn.BackColor = Color.FromArgb(152, 195, 121);
+        }
+
+        private void cdi_btn_Click(object sender, EventArgs e)
+        {
+            Guid gd = Guid.NewGuid();
+            Guid gd2 = Guid.NewGuid();
+            string drive_id = $"{ gd.ToString().Split('-')[3]}-{gd2.ToString().Split('-')[3]}";
+            Process pr = new Process();
+            pr.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            pr.StartInfo.FileName = @"exes\Volumeid.exe";
+            pr.StartInfo.Arguments = $"C: {drive_id}";
+            pr.StartInfo.UseShellExecute = false;
+            pr.StartInfo.CreateNoWindow = true;
+            pr.StartInfo.RedirectStandardOutput = true;
+            pr.StartInfo.RedirectStandardError = true;
+            string temp;
+            try
+            {
+                pr.Start();
+                temp = "";
+                while (!pr.HasExited)
+                {
+                    temp += pr.StandardOutput.ReadToEnd();
+                }
+            }
+            catch
+            {
+                drive_result.Text = "Did you move the exes folder ? Because it has been removed.";
+                cdi_btn.BackColor = Color.FromArgb(224, 107, 116);
+                return;
+            }
+            
+            if (temp.Contains("Access is denied"))
+            {
+                drive_result.Text = "Access is denied make sure you run the program in Admin";
+                cdi_btn.BackColor = Color.FromArgb(224, 107, 116);
+
+            }
+            else if(temp.Contains("updated to"))
+            {
+                drive_result.Text = "You have successfully updated you C Drive ID";
+                cdi_btn.BackColor = Color.FromArgb(152, 195, 121);
+
+            }
+
+        }
+
+        private void ccn_btn_Click(object sender, EventArgs e)
+        {
+            if (cname_textbox.Text.Trim() != "")
+            {
+                if (SetComputerNameA(cname_textbox.Text))
+                {
+                    drive_result.Text = "The Name Has Been Changed Sucessfully";
+                    ccn_btn.BackColor = Color.FromArgb(152, 195, 121);
+                }
+                else
+                {
+                    drive_result.Text = "Access is denied make sure you run the program in Admin";
+                    ccn_btn.BackColor = Color.FromArgb(224, 107, 116);
+
+                }
+            }
+            else
+            {
+                drive_result.Text = "Remove Any Space From The Name";
+                ccn_btn.BackColor = Color.FromArgb(224, 107, 116);
+            }
+        }
+
+        private void cguid_btn_Click(object sender, EventArgs e)
+        {
+            
+            try
+            {
+                string key = "HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Cryptography";
+                Guid newGuid = Guid.NewGuid();
+                Registry.SetValue(key,"MachineGuid", newGuid.ToString());
+                guidbox.Text = "Your MachineGuid : "+newGuid.ToString();
+                MessageBox.Show(newGuid.ToString());
+                cguid_btn.BackColor = Color.FromArgb(152, 195, 121);
+            }
+            catch
+            {
+                drive_result.Text = "Access is denied make sure you run the program in Admin";
+                cguid_btn.BackColor = Color.FromArgb(224, 107, 116);
+            }
+            
+        }
+    }
+}
